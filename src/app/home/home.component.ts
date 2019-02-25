0f94a51424d5e17ab1e42a450c41889b610ef7be
krishnakeerthi <krishnakeerthi675@gmail.com>
2019-02-25 10:47:12 -0500

    added info from api's and added styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+users: Object;
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getUsers().subscribe(data=>{
+      this.users=data
+      console.log(this.users)
+    })
   }
 
 }
